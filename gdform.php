1f4fb756064a1a9696a64624102b579548eee010
Jacqueline Reilly <jacquelineclementreilly@gmail.com>
2017-05-06 17:40:13 -0500

    Jacqueline Reilly Portfolio Site

---
+++ gdform.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Jacqueline Reilly Creative</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css" />
+	<!-- add meta data for SEO ex: keyboards, description and viewport, you'll need to add this to multiple pages that you want Google to pick up-->
+</head>
+
+<body>
+	<header>
+		<nav class="navigation">
+			<ul>
+				<li><a href="index.html">Work</a></li>
+				<li><a href="about.html">About</a></li>
+				<li><a href="resume.html">Resume</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+		</nav>
+		<div class="landing-page-transform">
+			<h1>Jacqueline Reilly</h1>
+			<h2>Aspiring Digital Creative</h2>
+		</div>
+	
+	</header>
+
+	<section id="work">
+		<h1>My Work</h1>
+		<article>
+			<div id="grid-container">
+				<div class="item">
+					<img src="img/purple_beau_expo.jpg" alt="Beautiful Exposure App" />
+					<a href="beautifulexposure.html"><div class="description">
+						<div>
+							<div>
+								<h3>Beautiful Exposure</h3>
+								<span>Digital</span>
+							</div>
+						</div>
+					</div></a>
+				</div>
+				<div class="item">
+					<img src="img/purple_hlo.jpg" alt="Hello Little One Beach Wedding Invitation" />
+					<a href="hlo.html"><div class="description">
+						<div>
+							<div>
+								<h3>Hello Little One</h3>
+								<span>Illustration, Print</span>
+							</div>
+						</div>
+					</div></a>
+				</div>
+				<div class="item">
+					<img src="img/purple_wired.jpg" alt="Wired Coffee Branding" />
+					<a href="wired.html"><div class="description">
+						<div>
+							<div>
+								<h3>Wired</h3>
+								<span>Identity</span>
+							</div>
+						</div>
+					</div></a>
+				</div>
+				<div class="item">
+					<img src="img/purple_gcm.jpg" alt="The Girl Code Movement Logo" />
+					<a href="gcm.html"><div class="description">
+						<div>
+							<div>
+								<h3>The Girl Code Movement</h3>
+								<span>Identity</span>
+							</div>
+						</div>
+					</div></a>
+				</div>
+				<div class="item">
+					<img src="img/purple_ak.jpg" alt="Anatolian Kitchen Branding and Identity" />
+					<a href="anatolian.html"><div class="description">
+						<div>
+							<div>
+								<h3>Antatolian Kitchen</h3>
+								<span>Identity</span>
+							</div>
+						</div>
+					</div></a>
+				</div>
+				<div class="item">
+					<img src="img/purple_typeillus.jpg" alt="Animal Type Illustrations" />
+					<a href="typeillus.html"><div class="description">
+						<div>
+							<div>
+								<h3>Typography Illustrations</h3>
+								<span>Illustration</span>
+							</div>
+						</div>
+					</div></a>
+				</div>
+				<div class="item">
+					<img src="img/purple_amped.jpg" alt="Amped iPad Publication" />
+					<a href="amped.html"><div class="description">
+						<div>
+							<div>
+								<h3>Amped Magazine</h3>
+								<span>Digital</span>
+							</div>
+						</div>
+					</div></a>
+				</div>
+				<div class="item">
+					<img src="img/purple_pizzahut.jpg" alt="Pizza Hut Advertising Campaign" />
+					<a href="pizzahut.html"><div class="description">
+						<div>
+							<div>
+								<h3>Pizza Hut</h3>
+								<span>Advertising</span>
+							</div>
+						</div>
+					</div></a>
+				</div>
+				<div class="item">
+					<img src="img/purple_typespec.jpg" alt="Misson Gothic Type Specimen" />
+					<a href="typespecimen.html"><div class="description">
+						<div>
+							<div>
+								<h3>Type Specimen</h3>
+								<span>Print</span>
+							</div>
+						</div>
+					</div></a>
+				</div>
+				<div class="item">
+					<img src="img/purple_arc.jpg" alt="Syracuse Arc of Onondaga Site Resign" />
+					<a href="arc.html"><div class="description">
+						<div>
+							<div>
+								<h3>Arc of Onondaga</h3>
+								<span>Campaign, Digital</span>
+							</div>
+						</div>
+					</div></a>
+				</div>
+				<div class="item">
+					<img src="img/purple_cityinternships.jpg" alt="City Internships Design Competition" />
+					<a href="cityinternships.html"><div class="description">
+						<div>
+							<div>
+								<h3>City Internships</h3>
+								<span>Campaign, Print</span>
+							</div>
+						</div>
+					</div></a>
+				</div>
+				<div class="item">
+					<a href="honey.html"><img src="img/purple_honey.jpg" alt="Honey's Truffles Branding" />
+					<div class="description">
+						<div>
+							<div>
+								<h3>Honey's Truffles</h3>
+								<span>Identity, Illustation</span>
+							</div>
+						</div>
+					</div></a>
+				</div>
+				<div class="item">
+					<img src="img/purple_vitale.jpg" alt="Ami Vitale iPad Photo Book" />
+					<a href="vitale.html"><div class="description">
+						<div>
+							<div>
+								<h3>Ami Vitale Photo Book</h3>
+								<span>Digital</span>
+							</div>
+						</div>
+					</div></a>
+				</div>
+			</div>
+		</article> 
+	</section>
+
+	<section id="about">
+		<div>
+		</div>
+		<h1>About Me</h1>
+		<article>
+			<div id="first-column">
+				<h1>My name is Jacqueline, but my friends call me Jackie. You can too.</h1>
+			</div>
+			<div id="second-column">
+				<p>I am a graphic design Senior at Syracuse University, grounded in the field of communication, keeping an emphasis on visual storytelling. As I pursue a design degree within the S.I. Newhouse School of Public Communications and a minor in psychology, I look forward to my unknown future, to telling stories that matter, and to constant learning and growing especially with design. My areas of interest fall in design psychology, visual branding, UI/UX design and front-end development.</p>
+			</div>
+		</article>
+		<article id="visual-description">
+			<h1>Description of Interests</h1>
+			<div>
+				<div>
+					<i class="fa fa-cogs"></i>
+				</div>
+				<div>
+					<p>Design Psychology</p>
+				</div>
+			</div>
+			<div>
+				<div>
+					<i class="fa fa-shield"></i>
+				</div>
+				<div>
+					<p>Visual Branding</p>
+				</div>
+			</div>
+			<div>
+				<div>
+					<i class="fa fa-sitemap"></i>
+				</div>
+				<div>
+					<p>UI/UX Design</p>
+				</div>
+			</div>
+			<div>
+				<div>
+					<i class="fa fa-code"></i>
+				</div>
+				<div>
+					<p>Front-End Development</p>
+				</div>
+			</div>
+		</article>
+	</section>
+
+	<section id="resume">
+		<h1>Resume</h1>
+		<article>
+			<div id="first-column">
+				<h1>I look forward to my unknown future and I am appreciative of those who have set me up for it.</h1>
+			</div>
+			<div id="second-column">
+				<p>Thus far, most of my experience falls under the realm of print design; however, innovation is driving the world toward a digital era. As I continue with my studies, I am adrenalized about creating design for all things print as well as for iOS applications, websites, and more. My work experience have only honed this interest in all areas of design. </p>
+				<p>I have been fortunate enough to work for companies such as <a href="http://www.leibowitz.co/" target="_blank">Leibowitz Branding and Design</a>, <br><a href"http://www.hellolittleone.com/" target="_blank">Hello Little One</a>, and <a href="http://www.clarksusa.com/" target="_blank">Clarks USA</a>. To see my full resume, click below.</p>
+				<a href="jacqueline-reilly-full-resume.pdf" target="_blank"><button>Download PDF</button></a>
+			</div>
+		</article>
+	</section>
+	
+	<section id="contact">
+		<div>
+		</div>
+		<h1>Contact Me</h1>
+		<article>
+			<div id="first-column">
+				<h1>Get In Touch!</h1>
+			</div>
+			<div id="second-column">
+				<?php
+				if (!isset($_POST['submit']))
+				{
+				?>	
+				<form action="index.php" method="post">
+				    <div>
+				        <input type="text" name="name" value="Name"/>
+				    </div>
+					<div>
+					    <input type="email" name="email" value="Email"/>
+					</div>
+					<div>
+				        <input type="text" name="name" value="Subject"/>
+				    </div>
+					<div>
+						<input name="message" value="All inquires and simple hellos are welcome."/>
+					</div>
+					<div class="button">
+						<button type="submit" name='submit'>Send your message</button>
+					</div>
+				</form>
+				<?php
+			}
+			else 
+			{
+				$name=$_POST['name'];
+				$email=$_POST['email'];
+				$message=$_POST['message'];
+				if (($name=="")||($email=="")||($message==""))
+				{
+					echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+				}
+				
+			else{
+				$from="From: $name<$email>\r\nReturn-path: $email";
+				$subject="Message from jacquelinereilly.com";
+				mail("jcreil02@syr.edu", $subject, $message, $from);
+				echo "Email sent!";
+				}
+			}
+		
+			?>
+			</div>
+		</article>
+	</section>
+
+	<footer>
+		<ul>
+			<li><a href ="https://www.linkedin.com/in/jclementreilly" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+			<li><a href ="https://twitter.com/jclementreilly" target="_blank"><i class="fa fa-twitter"></i></a></li>
+			<li><a href ="https://www.pinterest.com/jclementreilly" target="_blank"><i class="fa fa-pinterest-p"></i></a></li>
+			<li><a href ="https://www.instagram.com/jclementreilly" target="_blank"><i class="fa fa-instagram"></i></a></li>
+		</ul>	
+		<h1> &copy; 2016 Jacqueline Reilly. All rights reserved. | Syracuse, NY area </h1>
+
+	</footer>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/3.3.2/masonry.pkgd.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/3.2.0/imagesloaded.pkgd.min.js"></script>
+	<script src="js/site.js"></script>
+
+	<script>
+	$(document).ready(function() { 
+		var $grid = $('#grid-container').masonry({
+  			itemSelector: '.item',
+			percentPosition: true,
+			gutter: 20,
+		});
+
+		$grid.imagesLoaded().progress( function() {
+  			$grid.masonry('layout');
+		});
+	});
+	</script>
+	
+</body>
+
+</html>
